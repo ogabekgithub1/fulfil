458320518d13d3532428e5bfce88d4eaccbd40ad
ogabekgithub1 <89186444+ogabekgithub1@users.noreply.github.com>
2021-08-20 00:18:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let uzs = prompt("UZS da valyuta kiriting");
+
+let usd = uzs / 10654;
+let eur = uzs / 12674;
+
+let usd1 = Math.round(usd);
+let eur1 = Math.round(usd);
+alert(`${uzs} UZS = ${usd1} USD`);
+alert(`${uzs} UZS = ${eur1} EUR`);
+
+
